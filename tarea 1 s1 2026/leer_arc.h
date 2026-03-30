119fe65308bac98117530475bcc7e23d44a1ac16
ShiniNouzen <thedarksoulgamingyt@gmail.com>
2026-03-30 19:08:41 -0300

    Update leer_arc.h
    pequeños comentarios

--- tarea 1 s1 2026/leer_arc.h
+++ tarea 1 s1 2026/leer_arc.h
@@ -3,23 +3,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "Arr.h"
+#include "Arr.h"//incluyo mi librería/archivo Arr.h
 using namespace std;
-class arch{
+class arch{//clase para abrir el archivo
     public:
-    Arr arreglo;
-    void lectura(string nom_arch){
+    Arr arreglo;//llamo a la clase arreglo
+    void lectura(string nom_arch){//funcion de lectura del archivo
         ifstream file(nom_arch);
-        if(!file.is_open()){
+        if(!file.is_open()){//compruebo que haya abierto correctamente el archivo, de lo contrario da error y cierra el programa
             cerr<<"no se pudo abrir el archivo "<<nom_arch<<endl;
             exit(1);
         }
-        int valor;
+        int valor;//auxiliar para convertir los valores del archivo a enteros y pegarlos a la lista
         while(file >> valor && valor!=-1){
-            arreglo.append(valor);
+            arreglo.append(valor);//se añaden a la lista
         }
-        file.close();
-        return;
+        file.close();//se cierra el archivo para no tener leakeos de memoria
+        return;//retorna de la funcion void
     }
 };
 #endif // LEER_ARC_H
